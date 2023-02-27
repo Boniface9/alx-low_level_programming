93f33909f5dbf78d3037849ae51e2abc5dccb03d
Boniface <you@gmail.com>
2023-02-27 10:50:44 -0800

    Change made here

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "main.h"
+#include <string.h>
 
 /**
  * store the starting address of dest
  * copy each character from src to dest until we reach the null terminator
  * add the null terminator to the end of dest
  */
-char* copy_string(char* dest, const char* src)
+char *_strcpy(char *dest, const char *src)
 {
 char* dest_start = dest;
 while (*src != '\0')
@@ -16,5 +16,5 @@ dest++;
 src++;
 }
 *dest = '\0';
-return dest_start;
+return (dest_start);
 }
