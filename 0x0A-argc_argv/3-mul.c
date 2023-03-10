6951ecaf9d958a1e6e7b38d15b79a698e409c395
Boniface <you@gmail.com>
2023-03-10 14:59:23 -0800

    Changes made

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,6 +7,8 @@
  * @argv: array of arguments
  * Return: 0 if successful, 1 if error
  */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 
 int main(int argc, char *argv[])
 {
@@ -16,8 +18,6 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-a = atoi(argv[1]);
-b = atoi(argv[2]);
 product = (a * b);
 printf("%d\n", product);
 return (0);
