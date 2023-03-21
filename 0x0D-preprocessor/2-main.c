97353e532e3b271f11404a970d7e6e01378403c5
Boniface <you@gmail.com>
2023-03-21 06:37:50 -0700

    Changes made

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/*
+ * FILENAME: 2-main.c
+ *
+ * DESCRIPTION:
+ * This program prints the name of the file
+ * it was compiled from using the __FILE__ macro.
+ */
+
 int main(void)
 {
     printf("%s\n", __FILE__);
     return 0;
 }
-
