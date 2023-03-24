2d534a8de1601afd8da8ef971e05d526d7b75ff2
Boniface <you@gmail.com>
2023-03-24 10:56:54 -0700

    Changes made here

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
-void print_name(char *name, void (*f)(char *)) {
-  f(name);
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
+void print_name_as_is(char *name)
+{
+printf("Hello, my name is %s\n", name);
+}
+
+void print_name_uppercase(char *name)
+{
+}
+
+int main(void)
+{
+print_name("Alice", print_name_as_is);
+print_name("Bob", print_name_uppercase);
+return (0);
 }
