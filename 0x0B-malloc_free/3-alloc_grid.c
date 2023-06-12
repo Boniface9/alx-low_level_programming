0017df731436998f4092fd5d88ac7e315f94b75c
Boniface <bonito@gmmail.com>
2023-06-12 14:39:51 -0700

    Changes have been made

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,8 @@
 
 int **alloc_grid(int width, int height)
 {
+int i, j;
+
 if (width <= 0 || height <= 0)
 {
 return (NULL);
@@ -21,19 +23,19 @@ if (grid == NULL)
 {
 return (NULL);
 }
-for (int i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
 grid[i] = malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
-for (int j = 0; j < i; j++)
+for (j = 0; j < i; j++)
 {
 free(grid[j]);
 }
 free(grid);
 return (NULL);
 }
-for (int j = 0; j < width; j++)
+for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
 }
