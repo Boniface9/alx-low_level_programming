e4ee87650b5147b50f7f1255c10fbc370cc6c4d2
Boniface <bonito@gmmail.com>
2023-06-15 14:07:05 -0700

    Changes made here

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,48 +17,59 @@
 int isNumber(char *str);
 int _strlen(char *str);
 void multiply(char *num1, char *num2);
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 char *num1;
 char *num2;
-if (argc != 3) {
+if (argc != 3)
+{
 printf("Error\n");
 return (98);
 }
 num1 = argv[1];
 num2 = argv[2];
-if (!isNumber(num1) || !isNumber(num2)) {
+if (!isNumber(num1) || !isNumber(num2))
+{
 printf("Error\n");
 return (98);
 }
 multiply(num1, num2);
 return (0);
 }
-int isNumber(char *str) {
+int isNumber(char *str)
+{
 int i = 0;
-while (str[i] != '\0') {
-if (str[i] < '0' || str[i] > '9') {
+while (str[i] != '\0')
+{
+if (str[i] < '0' || str[i] > '9')
+{
 return (0);
 }
 i++;
 }
 return (1);
 }
-int _strlen(char *str) {
+int _strlen(char *str)
+{
 int len = 0;
-while (str[len] != '\0') {
+while (str[len] != '\0')
+{
 len++;
 }
 return (len);
 }
-void multiply(char *num1, char *num2) {
+void multiply(char *num1, char *num2)
+{
 int len1 = _strlen(num1);
 int len2 = _strlen(num2);
 int lenResult = len1 + len2;
 int *result = calloc(lenResult, sizeof(int));
 int i, j;
-int start = 0; /* Move the declaration here */
-for (i = len1 - 1; i >= 0; i--) {
-for (j = len2 - 1; j >= 0; j--) {
+int start = 0;
+for (i = len1 - 1; i >= 0; i--)
+{
+for (j = len2 - 1; j >= 0; j--)
+{
 int n1 = num1[i] - '0';
 int n2 = num2[j] - '0';
 int product = n1 * n2;
@@ -69,14 +80,18 @@ result[pos2] = sum % 10;
 result[pos1] += sum / 10;
 }
 }
-while (start < lenResult && result[start] == 0) {
+while (start < lenResult && result[start] == 0)
+{
 start++;
 }
-if (start == lenResult) {
+if (start == lenResult)
+{
 printf("0\n");
 }
-else{
-for (i = start; i < lenResult; i++) {
+else
+{
+for (i = start; i < lenResult; i++)
+{
 printf("%d", result[i]);
 }
 printf("\n");
