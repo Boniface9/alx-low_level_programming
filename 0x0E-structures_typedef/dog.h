d0b21872b39d9b8975394f7dda30f5e8e8dc8f6d
Boniface <bonito@gmmail.com>
2023-06-20 14:07:33 -0700

    Changes made

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
 /*struct dog - a struct representing a dog
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
  * Description: a structure representing a dog.
  */
+
 typedef struct dog
 {
 har *name;
@@ -15,4 +17,5 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif /* DOG_H */
