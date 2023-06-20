9c0ebce7cc460fc36c9205c6395763fe4da34875
Boniface <bonito@gmmail.com>
2023-06-20 13:44:45 -0700

    Changes made

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,10 +11,11 @@
  */
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
+har *name;
+float age;
+char *owner;
+}
+dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
