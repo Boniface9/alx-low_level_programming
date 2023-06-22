e615342254c9ab5df6d5ac0612ffa8c59d596136
Boniface <bonito@gmmail.com>
2023-06-22 09:13:31 -0700

    Changes made

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,13 @@
  * print_name - prints a name using a given function
  * @name: the name to be printed
  * @f: pointer to the function that prints the name
- *
  * Return: None
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f){
+if
+(name && f)
+{
 f(name);
 }
 }
