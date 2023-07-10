5bd414bc0717cacb9bb9dce997628ccd4b2e72a5
Boniface <bonito@gmmail.com>
2023-07-10 07:55:02 -0700

    Changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,40 +3,42 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include "main.h"
+
 /**
- * create_file - Creates a file with the specified name
- * and writes the provided content to it.
- * @filename: The name of the file to create.
- * @text_content: The NULL-terminated string to write to the file.
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
  *
- * Return: 1 on success, -1 on failure.
- * If the file already exists, truncate it.
- * If filename is NULL, return -1.
- * If text_content is NULL, create an empty file.
- * The created file has permissions rw-------.
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+size_t i;
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
+
+/**
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, bytes_written, text_length = 0;
+int fd;
+ssize_t len;
 if (filename == NULL)
 return (-1);
-if (text_content != NULL)
-{
-while (text_content[text_length])
-text_length++;
-}
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-if (text_length > 0)
-{
-bytes_written = write(fd, text_content, text_length);
-if (bytes_written == -1 || bytes_written != text_length)
-{
+if (text_content != NULL)
+len = write(fd, text_content, _strlen(text_content));
 close(fd);
+if (len == -1)
 return (-1);
-}
-}
-close(fd);
 return (1);
 }
