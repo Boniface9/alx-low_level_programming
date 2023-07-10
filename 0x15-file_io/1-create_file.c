1978498b00da125adb3c5909ed56869bbecaca1e
Boniface <bonito@gmmail.com>
2023-07-10 08:06:47 -0700

    Changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,31 +17,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written, text_length = 0;
-
-    if (filename == NULL)
-        return -1;
-
-    if (text_content != NULL)
-    {
-        while (text_content[text_length])
-            text_length++;
-    }
-
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return -1;
-
-    if (text_length > 0)
-    {
-        bytes_written = write(fd, text_content, text_length);
-        if (bytes_written == -1 || bytes_written != text_length)
-        {
-            close(fd);
-            return -1;
-        }
-    }
-
-    close(fd);
-    return 1;
+int fd, bytes_written, text_length = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+while (text_content[text_length])
+text_length++;
+}
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (text_length > 0)
+{
+bytes_written = write(fd, text_content, text_length);
+if (bytes_written == -1 || bytes_written != text_length)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
 }
